cdd60bc41761bd5c89ced26cae1f835aca2035c6
Victor Segura <vseguranes@gmail.com>
2026-03-04 09:56:00 +0100

    feat(Superdome): implementación método buscarTeletubbi

--- src/Superdome.java
+++ src/Superdome.java
@@ -17,4 +17,24 @@ public class Superdome {
             teletubbies = new Teletubbi[15];
         }
     }
+
+    /**
+     * Metodo que busca por id el teletubbi buscado
+     * @param id
+     * @return Teletubbi teletibbiBuscado si lo encuentra o null si no lo ha encontrado
+     */
+    public Teletubbi buscarTeletubbi(int id){
+        boolean seguirBuscando = true;
+        Teletubbi teletubbiBusacado = null;
+
+        for( int i = 0; i < NUM_MAX_TELETUBBIES && seguirBuscando; i++){
+            if( teletubbies[i] != null && teletubbies[i].getId() == id){
+                seguirBuscando = false;
+                teletubbiBusacado = teletubbies[i];
+            }
+        }
+        return teletubbiBusacado;
+    }
+
+
 }
